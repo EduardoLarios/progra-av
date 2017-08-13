b3806899e7421278f450c583f7f0269bb9e7918a
adal347 <adalaidc@acm.org>
2017-08-12 21:05:04 -0500

    Set up

---
+++ homeworks/hw_one/sorts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "sorts.h"
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
---
+++ homeworks/hw_one/sorts.h
@@ -0,0 +1,20 @@
+/*
+ * sorts.h
+ */
+
+#ifndef SORTS_H_
+#define SORTS_H_
+
+int* count_sort(int *arr, int size) {
+	return arr;
+}
+
+int* bucket_sort(int *arr, int size) {
+	return arr;
+}
+
+int* merge_sort(int *arr, int size) {
+	return arr;
+}
+
+#endif /* SORTS_H_ */
