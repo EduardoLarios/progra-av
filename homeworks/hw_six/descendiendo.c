3228e10ba882186aa903c891cd09db0c222bc4a5
adal <adita0898@gmail.com>
2017-09-25 00:02:43 +0000

    Project

--- homeworks/hw_six/descendiendo.c
+++ homeworks/hw_six/descendiendo.c
@@ -4,12 +4,54 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void child_process(int i, int level, char* program);
+
+void create_process(int i, int level, char* program) {
+	int pid, j;
+	
+	if ( (pid = fork()) < 0 ) {
+		perror(program);
+		exit(-1);
+	} else if (pid == 0) {
+		child_process(++i, level, program);
+	} else {
+		for (j = 0; j < i; j++) {
+			printf("\t");
+		}
+		printf("PPID = %i PID = %i LEVEL = %i\n", getppid(), getpid(), i);
+		wait(NULL);
+		exit(0);
+	}
+}
+
+void child_process(int i, int level, char* program) {
+    int pid, j;
+	
+	if (i == 0) {
+		create_process(i, level, program);
+	} else if (i != (level + 1)) {
+		for (j = 0; j < i; j++) {
+			create_process(i, level, program);
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
+    int pid, i, level;
+    long ps;
     
     if (argc != 2) {
 		fprintf(stderr, "Forma de uso: %s num\n", argv[0]);
 		return -1;
 	}
+	
+	level = atoi(argv[1]);
+	if (level <= 0) {
+		fprintf(stderr, "Error: el programa recibe un numero entero positivo mayor a 0\n");
+		return -1;
+	}
+	
+	create_process(0, level, argv[0]);
 	
     return 0;
 }
--- projects/project_one/rotacion.c
+++ projects/project_one/rotacion.c
@@ -1,8 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+void rotation(char* directory, char* name, char* program, int number, int i) {
+	FILE *file;
+	FILE *new_file;
+	char filename[NAME_MAX + 1], new_filename[NAME_MAX + 1], character;
+	int j = 0;
+
+	for (int j = 0; j < i; j++)
+	{
+		if (i == 0) sprintf(filename, "%s/%s.log", directory, name);
+		else sprintf(filename, "%s/%s.%i", directory, name, j);
+		sprintf(new_filename, "%s/%s.%i", directory, name, j + 1);
+
+		file = fopen(filename, "r");
+		new_file = fopen(new_filename, "w");
+		if (file == NULL) {
+			perror(program);
+			exit(-1);
+		}
+
+		if (new_file == NULL) {
+			perror(program);
+			exit(-1);
+		}
+		
+		while ( fscanf(file, "%c", &character) != EOF ) fprintf(new_file, "%c", character);
+
+		if (i != number)
+		{
+		    fclose(file);
+	    	fclose(new_file);
+			rotation(directory, name, program, number, ++i);
+		} else {
+	    	fclose(file);
+		    fclose(new_file);
+			remove(new_filename);
+			sprintf(new_filename, "%s/%s.log", directory, name);
+			new_file = fopen(new_filename, "w");
+		}
+	}
+}
+
+void open_directory(char* directory, char* program, char* log, int time, int number) {
+	DIR *dir;
+	struct dirent *dir_entry;
+	char filename[NAME_MAX + 1];
+	int exists = 0;
+
+	if( (dir = opendir(directory)) == NULL) {
+		perror(program);
+		exit(-1);
+	}
+	
+	
+	sprintf(filename, "%s.log", log);
+	while ( (dir_entry = readdir(dir)) != NULL) {
+		if (strcmp(dir_entry->d_name, ".") == 0 ||
+			strcmp(dir_entry->d_name, "..")  == 0)
+			continue;
+		
+		if (strcmp(dir_entry->d_name, filename) == 0) exists = 1;
+	}
+
+	if (exists)
+	{
+ 		sleep(time);
+		rotation(directory, log, program, number, 0);
+	} else fprintf(stderr, "Error: el archivo %s no existe\n", log);
+	
+	closedir(dir);
+}
 
 int main(int argc, char const *argv[]) {
-  
-  return 0;
+	int number, time;
+	if (argc != 5)
+	{
+		fprintf(stderr, "Forma de uso: %s directorio_log archivo_log numero tiempo\n", argv[0]);
+		return -1;
+	}
+
+	number = atoi(argv[3]);
+	time = atoi(argv[4]);
+	if (number <= 0 || time <= 0)
+	{
+		fprintf(stderr, "Error: el programa recibe un numero entero positivo mayor a 0\n");
+		return -1;
+	}
+
+	open_directory(argv[1], argv[0], argv[2], time, number);
+
+	return 0;
 }
