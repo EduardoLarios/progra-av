ecadf14bc8b1820da30822a5fa79d7efe40d4e91
adal <adita0898@gmail.com>
2017-10-24 13:57:33 +0000

    sessions

---
+++ sessions/twenty-three/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+typedef struct buffer {
+    
+}
