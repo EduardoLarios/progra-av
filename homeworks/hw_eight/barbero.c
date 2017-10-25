255e968d0afdde344027bc295e323e14796e8c71
adal347 <A01270898@itesm.mx>
2017-10-25 13:39:53 -0500

    comment functions

--- homeworks/hw_eight/barbero.c
+++ homeworks/hw_eight/barbero.c
@@ -1,38 +1,38 @@
 #include "header.h"
 
-void a_producer(char* program) {
-	int i, k, semid;
-	key_t key;
-
-	if ( (key = ftok("/dev/null", 125)) == (key_t) -1 ) {
-		perror(program);
-		exit(-1);
-	}
-
-	if ( (semid = semget(key, 4, 0666)) < 0 ) {
-		perror(program);
-		exit(-1);
-	}
-
-	srand( getpid() );
-	for (k = 0; k < 10; k++) {
-		printf("Producer %i trying to access the buffer.\n", getpid());
-		sem_wait(semid, PRODUCERS, 1);
-		printf("Producer %i accesing the buffer.\n", getpid());
-
-		i = (rand() % 5) + 1;
-		printf("Producer %i trying to put %i product(s) - spaces = %i\n", getpid(), i, semctl(semid, SPACES, GETVAL, 0));
-		sem_wait(semid, SPACES, i);
-		printf("Producer %i put their products.\n", getpid());
-		sem_signal(semid, ITEMS, i);
-
-		sem_signal(semid, PRODUCERS, 1);
-		printf("Producer %i is going to sleep.\n", getpid());
-
-		sleep((rand() % 10) + 1);
-	}
-	exit(0);
-}
+// void a_producer(char* program) {
+// 	int i, k, semid;
+// 	key_t key;
+//
+// 	if ( (key = ftok("/dev/null", 125)) == (key_t) -1 ) {
+// 		perror(program);
+// 		exit(-1);
+// 	}
+//
+// 	if ( (semid = semget(key, 4, 0666)) < 0 ) {
+// 		perror(program);
+// 		exit(-1);
+// 	}
+//
+// 	srand( getpid() );
+// 	for (k = 0; k < 10; k++) {
+// 		printf("Producer %i trying to access the buffer.\n", getpid());
+// 		sem_wait(semid, PRODUCERS, 1);
+// 		printf("Producer %i accesing the buffer.\n", getpid());
+//
+// 		i = (rand() % 5) + 1;
+// 		printf("Producer %i trying to put %i product(s) - spaces = %i\n", getpid(), i, semctl(semid, SPACES, GETVAL, 0));
+// 		sem_wait(semid, SPACES, i);
+// 		printf("Producer %i put their products.\n", getpid());
+// 		sem_signal(semid, ITEMS, i);
+//
+// 		sem_signal(semid, PRODUCERS, 1);
+// 		printf("Producer %i is going to sleep.\n", getpid());
+//
+// 		sleep((rand() % 10) + 1);
+// 	}
+// 	exit(0);
+// }
 
 int main(int argc, char* argv[]) {
 	int amount, pid, i;
--- homeworks/hw_eight/cliente.c
+++ homeworks/hw_eight/cliente.c
@@ -1,38 +1,38 @@
 #include "header.h"
 
-void a_consumer(char* program) {
-	int i, k, semid;
-	key_t key;
-
-	if ( (key = ftok("/dev/null", 125)) == (key_t) -1 ) {
-		perror(program);
-		exit(-1);
-	}
-
-	if ( (semid = semget(key, 4, 0666)) < 0 ) {
-		perror(program);
-		exit(-1);
-	}
-
-	srand( getpid() );
-	for (k = 0; k < 10; k++) {
-		printf("Consumer %i trying to access the buffer.\n", getpid());
-		sem_wait(semid, CONSUMERS, 1);
-		printf("Consumer %i accesing the buffer.\n", getpid());
-
-		i = (rand() % 5) + 1;
-		printf("Consumer %i trying to get %i product(s) - items = %i\n", getpid(), i, semctl(semid, ITEMS, GETVAL, 0));
-		sem_wait(semid, ITEMS, i);
-		printf("Consumer %i took their products.\n", getpid());
-		sem_signal(semid, SPACES, i);
-
-		sem_signal(semid, CONSUMERS, 1);
-		printf("Consumer %i is going to sleep.\n", getpid());
-
-		sleep((rand() % 10) + 1);
-	}
-	exit(0);
-}
+// void a_consumer(char* program) {
+// 	int i, k, semid;
+// 	key_t key;
+//
+// 	if ( (key = ftok("/dev/null", 125)) == (key_t) -1 ) {
+// 		perror(program);
+// 		exit(-1);
+// 	}
+//
+// 	if ( (semid = semget(key, 4, 0666)) < 0 ) {
+// 		perror(program);
+// 		exit(-1);
+// 	}
+//
+// 	srand( getpid() );
+// 	for (k = 0; k < 10; k++) {
+// 		printf("Consumer %i trying to access the buffer.\n", getpid());
+// 		sem_wait(semid, CONSUMERS, 1);
+// 		printf("Consumer %i accesing the buffer.\n", getpid());
+//
+// 		i = (rand() % 5) + 1;
+// 		printf("Consumer %i trying to get %i product(s) - items = %i\n", getpid(), i, semctl(semid, ITEMS, GETVAL, 0));
+// 		sem_wait(semid, ITEMS, i);
+// 		printf("Consumer %i took their products.\n", getpid());
+// 		sem_signal(semid, SPACES, i);
+//
+// 		sem_signal(semid, CONSUMERS, 1);
+// 		printf("Consumer %i is going to sleep.\n", getpid());
+//
+// 		sleep((rand() % 10) + 1);
+// 	}
+// 	exit(0);
+// }
 
 int main(int argc, char* argv[]) {
 	int amount, pid, i;
