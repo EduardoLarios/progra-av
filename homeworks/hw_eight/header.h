1c56ec1dbf59051df380f1e3fb69677952aa9bfe
adal347 <A01270898@itesm.mx>
2017-11-03 09:31:24 -0600

    Start Server-Client

---
+++ homeworks/hw_eight/header.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define DEFAULT_PORT    9999
+#define DEFAULT_IP      "127.0.0.1"
+
+#endif
