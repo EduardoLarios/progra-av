f57af34a49f3983abbafe13b694ff9f20f3e98ae
adal347 <A01270898@itesm.mx>
2017-11-05 13:44:10 -0600

    client

--- homeworks/hw_nine/client.c
+++ homeworks/hw_nine/client.c
@@ -1,4 +1,27 @@
 #include "header.h"
+#include <time.h>
+
+int mi_table[4][4];
+int oponent_table[4][4];
+
+void display(int table[4][4]) {
+  int i, j;
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      printf("[%i]", table[i][j]);
+    }
+    printf("\n");
+  }
+}
+void create_table() {
+  int i, j;
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      mi_table[i][j] = 0;
+      oponent_table[i][j] = 0;
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
   int sfd, game = 1, answer;
@@ -17,35 +40,38 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	if ( (sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-		perror(argv[0]);
-		return -1;
-	}
-
-	server_info.sin_family = AF_INET;
-	server_info.sin_addr.s_addr = inet_addr(argv[1]);
-	server_info.sin_port = htons(port);
-	if ( connect(sfd, (struct sockaddr *) &server_info, sizeof(server_info)) < 0 ) {
-		perror(argv[0]);
-		return -1;
-	}
+  create_table();
+  display(mi_table);
+  printf("\n");
+  display(oponent_table);
+	// if ( (sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+	// 	perror(argv[0]);
+	// 	return -1;
+	// }
+  //
+	// server_info.sin_family = AF_INET;
+	// server_info.sin_addr.s_addr = inet_addr(argv[1]);
+	// server_info.sin_port = htons(port);
+	// if ( connect(sfd, (struct sockaddr *) &server_info, sizeof(server_info)) < 0 ) {
+	// 	perror(argv[0]);
+	// 	return -1;
+	// }
 
   printf("Give me your nick name: ");
   gets(&nickname);
-  write(sfd, &nickname, sizeof(nickname));
-  
-  while (game) {
-
-    gets(&play)
-    write(sfd, &play, sizeof(play));
-
-    read(sfd, &answer, sizeof(answer));
-		switch (answer) {
-			case HIT: printf("You sank a ship.\n"); break;
-			case NOTHING: printf("Nothing happend.\n"); break;
-			default     : printf("You win\n"); game = 1; break;
-		}
-	}
-	close(sfd);
+  // write(sfd, &nickname, sizeof(nickname));
+  // while (game) {
+  //
+  //   gets(&play)
+  //   write(sfd, &play, sizeof(play));
+  //
+  //   read(sfd, &answer, sizeof(answer));
+	// 	switch (answer) {
+	// 		case HIT: printf("You sank a ship.\n"); break;
+	// 		case NOTHING: printf("Nothing happend.\n"); break;
+	// 		default     : printf("You win\n"); game = 1; break;
+	// 	}
+	// }
+	// close(sfd);
 	return 0;
 }
