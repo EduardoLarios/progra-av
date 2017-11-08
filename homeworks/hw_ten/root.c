28bd18cf990a4d7ccd290e2f2b6bab76cdd516a9
adal347 <A01270898@itesm.mx>
2017-11-08 17:18:56 -0600

    solved

--- homeworks/hw_ten/root.c
+++ homeworks/hw_ten/root.c
@@ -4,20 +4,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
+
+#define SIZE 100
+
+double array[SIZE];
 
 void square_root() {
-  printf("Finished\n");
+  double i;
+
+  for (i = 0; i < 100; i++) {
+    array[i] = sqrt(i);
+  }
 };
 
 void* task(void* param) {
   int thread = *( (int *) param );
+  int i;
   if (thread) {
     square_root();
   } else {
     printf("Wait for the result\n");
+    if (array[SIZE - 1]) {
+      for (i = 0; i < 100; i++) {
+        printf("[ %d ]\n", array[i]);
+      }
+    }
   }
 
 	return ( (void*) 0);
