029faa0f0812659edde64b325e13f4c9f75c20a3
adal347 <A01270898@itesm.mx>
2017-11-13 18:23:16 -0600

    project

---
+++ projects/project_two/header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+
+#define DEFAULT_PORT    9999
+#define DEFAULT_IP      "127.0.0.1"
+
+#endif
---
+++ projects/project_two/server.c
@@ -0,0 +1,61 @@
+#include "header.h"
+#include <string.h>
+
+#define SIZE 100
+
+void* serves_client(void *param) {
+	int nsfd = *( (int*) param);
+
+  printf("PID = %i\n", getpid());
+  // como enviar dos parametros en uno
+	close(nsfd);
+}
+
+void server(char* ip, int port, char* ruta, char* program) {
+	int sfd, nsfd, pid;
+	pthread_t thread_id;
+	struct sockaddr_in server_info, client_info;
+
+	if ( (sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+		perror(program);
+		exit(-1);
+	}
+
+	server_info.sin_family = AF_INET;
+	server_info.sin_addr.s_addr = inet_addr(ip);
+	server_info.sin_port = htons(port);
+	if ( bind(sfd, (struct sockaddr *) &server_info, sizeof(server_info)) < 0 ) {
+		perror(program);
+		exit(-1);
+	}
+
+	listen(sfd, 1);
+	while (1) {
+		int len = sizeof(client_info);
+		if ( (nsfd = accept(sfd, (struct sockaddr *) &client_info, &len)) < 0 ) {
+			perror(program);
+			exit(-1);
+		}
+
+		pthread_create(&thread_id, NULL, serves_client, ((void *) &nsfd));
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	char ruta[SIZE], ip[15];
+  int port;
+
+  strcpy(ip, DEFAULT_IP);
+  port = DEFAULT_PORT;
+	if (argc != 2) {
+    printf("Usage: %s [dir]\n", argv[0]);
+		return -1;
+	}
+
+  ruta = argv[1];
+
+	server(ip, port, ruta, argv[0]);
+
+	return 0;
+}
