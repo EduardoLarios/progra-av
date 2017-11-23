661e2dde90ac519f5e6b8ce176a8d6ba56032462
adal <adita0898@gmail.com>
2017-11-23 23:50:59 +0000

    hw1

--- homeworks/hw_one/exercise1.c
+++ homeworks/hw_one/exercise1.c
@@ -17,47 +17,64 @@ int size;
 
 /* Regresa la distancia entre dos puntos dados (p1, p2) */
 float distance(const Point *p1, const Point *p2) {
-	return sqrt( ((p2->x - p1->x) * (p2->x - p1->x)) + 
+	return sqrt( ((p2->x - p1->x) * (p2->x - p1->x)) +
 		         ((p2->y - p1->y) * (p2->y - p1->y)) );
 }
 
-/* A IMPLEMENTAR
-   Carga los datos de entrada entandar (consola). Primero 
+/* Carga los datos de entrada entandar (consola). Primero
    recibe la cantidad de elementos a leer (size), seguido por
    por 'size' pares de flotantes, los puntos a leer */
 void load_data() {
 	int i;
 	float x, y;
+
+	scanf("%i", &size);
+	points = (Point*) malloc(size * sizeof(Point));
+
+	for (i = 0; i < size; i++) {
+		scanf("%f %f", &x, &y);
+		points[i].x = x;
+		points[i].y = y;
+	}
 }
 
 
-/* A IMPLEMENTAR
-   Regresa el apuntador al punto leído (points) más 
+/* Regresa el apuntador al punto leído (points) más
    cercano a p */
 Point* get_near_from(const Point* p) {
-	Point *near = NULL;
-	
+	Point *near = &points[0];
+	int i;
+
+	for (i = 1; i < size; i++) {
+		if (distance(p, &points[i]) < distance(p, near)) {
+			near = &points[i];
+		}
+	}
+
 	return near;
 }
 
 
 /* A IMPLEMENTAR
-   Regresa un arreglo de puntos (array) conteniendo los 
+   Regresa un arreglo de puntos (array) conteniendo los
    últimos num elementos del arreglo points. */
 Point* get_last_elements(int num) {
-	Point* array = NULL;
-	
+	Point* array = &points[size - num];
+
 	return array;
 }
 
 
-/* A IMPLEMENTAR
-   Recibe un arreglo de puntos cualesquiera (array)y la cantidad 
-   de elementos a desplegar (num). Despliega los elementos en el 
+/* Recibe un arreglo de puntos cualesquiera (array)y la cantidad
+   de elementos a desplegar (num). Despliega los elementos en el
    formanto '(x, y)' */
 void display_points(Point *array, int num) {
 	int i;
 
+	for (i = 0; i < num; i++) {
+		printf("(%.2f, %.2f) ", array[i].x, array[i].y);
+	}
+	printf("\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -76,6 +93,5 @@ int main(int argc, char* argv[]) {
 	display_points(lasts, 10);
 
 	free(points);
-	free(lasts);
 	return 0;
 }
