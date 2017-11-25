d72ebbdad1766cf742a27c76eb602fd4819bf1d2
adal347 <A01270898@itesm.mx>
2017-11-25 17:22:00 -0600

    cifrado

--- homeworks/cifrado_de_un_archivo/cifrado.c
+++ homeworks/cifrado_de_un_archivo/cifrado.c
@@ -4,53 +4,41 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-#define SIZE 128
-
 int main(int argc, char* argv[]) {
-	int fd_in, fd_out, bytes;
-	char buffer[SIZE];
-	
-	int fd_three;
-	unsigned long int lol;
-    
-    if (argc != 4) {
+	int fd_in, fd_out, bytes, n;
+	unsigned char buffer, aux, aux2;
+
+  if (argc != 4) {
 		fprintf(stderr, "Forma de uso: %s numero origen destino\n", argv[0]);
 		return -1;
 	}
-	
-	if (atoi(argv[1]) <= 0) {
+
+	n = atoi(argv[1]);
+	if (n <= 0) {
 		fprintf(stderr, "Error: el primer parámetro debe ser un numero entero positivo.\n");
 	    return -1;
 	}
-	
+
 	if ( (fd_in = open(argv[2], O_RDONLY)) < 0 ) {
 		fprintf(stderr, "Error: el archivo %s no existe.\n", argv[2]);
 		return -1;
 	}
-	
+
 	if ( (fd_out = open(argv[3], O_WRONLY | O_TRUNC | O_CREAT, 0666)) < 0 ) {
 		perror(argv[0]);
 		return -1;
 	}
-	
-	if ( (fd_three = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0666)) < 0 ) {
-		perror(argv[0]);
-		return -1;
+
+	while ( (bytes = read(fd_in, &buffer, sizeof(char))) != 0 ) {
+		aux = buffer<<n;
+		aux2 = buffer>>n;
+		buffer = aux|aux2;
+		write(fd_out, &buffer, bytes);
 	}
-	
-	lseek(fd_in, atoi(argv[1]) * -1, SEEK_END);
-	while ( (bytes = read(fd_in, buffer, SIZE * sizeof(char))) != 0 ) {
-		write(fd_out, buffer, bytes);
-	}
-	
-	lseek(fd_in, 0, SEEK_SET);
-	while ( (bytes = read(fd_in, buffer, SIZE * sizeof(char))) != 0 ) {
-		write(fd_out, buffer, bytes);
-	}
-	
-	
+
+
 	close(fd_in);
 	close(fd_out);
-	
+
 	return 0;
 }
