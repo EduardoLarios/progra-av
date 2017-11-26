4fb20f13c4d71515e3cec4f0f2dd382aa7f503e8
adal347 <A01270898@itesm.mx>
2017-11-25 18:13:58 -0600

    Cifrado

--- homeworks/cifrado_de_un_archivo/cifrado.c
+++ homeworks/cifrado_de_un_archivo/cifrado.c
@@ -4,9 +4,11 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
+
 int main(int argc, char* argv[]) {
 	int fd_in, fd_out, bytes, n;
-	unsigned char buffer, aux, aux2;
+	unsigned char buffer, aux;
+	unsigned long length_text, i = 0;
 
   if (argc != 4) {
 		fprintf(stderr, "Forma de uso: %s numero origen destino\n", argv[0]);
@@ -29,13 +31,20 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	lseek(fd_in, n * -1, SEEK_END);
 	while ( (bytes = read(fd_in, &buffer, sizeof(char))) != 0 ) {
-		aux = buffer<<n;
-		aux2 = buffer>>n;
-		buffer = aux|aux2;
 		write(fd_out, &buffer, bytes);
 	}
 
+	length_text = lseek(fd_in, 0, SEEK_END);
+	lseek(fd_in, 0, SEEK_SET);
+	length_text -= n;
+	while ( (bytes = read(fd_in, &buffer, sizeof(char))) != 0 ) {
+		if (length_text > i) {
+			write(fd_out, &buffer, bytes);
+		}
+		i++;
+	}
 
 	close(fd_in);
 	close(fd_out);
