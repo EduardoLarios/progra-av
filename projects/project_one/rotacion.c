0a1f0270da96636e4f0fdc088fa4ceb6c4872897
adal347 <A01270898@itesm.mx>
2017-11-25 18:40:21 -0600

    rotacion

--- projects/project_one/rotacion.c
+++ projects/project_one/rotacion.c
@@ -9,9 +9,9 @@ void rotation(char* directory, char* name, char* program, int number, int i) {
 	FILE *file;
 	FILE *new_file;
 	char filename[NAME_MAX + 1], new_filename[NAME_MAX + 1], character;
-	int j = 0;
+	int j;
 
-	for (int j = 0; j < i; j++)
+	for (j = 0; j < i; j++)
 	{
 		if (i == 0) sprintf(filename, "%s/%s.log", directory, name);
 		else sprintf(filename, "%s/%s.%i", directory, name, j);
@@ -28,7 +28,7 @@ void rotation(char* directory, char* name, char* program, int number, int i) {
 			perror(program);
 			exit(-1);
 		}
-		
+
 		while ( fscanf(file, "%c", &character) != EOF ) fprintf(new_file, "%c", character);
 
 		if (i != number)
@@ -46,7 +46,7 @@ void rotation(char* directory, char* name, char* program, int number, int i) {
 	}
 }
 
-void open_directory(char* directory, char* program, char* log, int time, int number) {
+void open_directory(char* directory, char* program, char* l, int t, int number) {
 	DIR *dir;
 	struct dirent *dir_entry;
 	char filename[NAME_MAX + 1];
@@ -56,28 +56,28 @@ void open_directory(char* directory, char* program, char* log, int time, int num
 		perror(program);
 		exit(-1);
 	}
-	
-	
-	sprintf(filename, "%s.log", log);
+
+
+	sprintf(filename, "%s.log", l);
 	while ( (dir_entry = readdir(dir)) != NULL) {
 		if (strcmp(dir_entry->d_name, ".") == 0 ||
 			strcmp(dir_entry->d_name, "..")  == 0)
 			continue;
-		
+
 		if (strcmp(dir_entry->d_name, filename) == 0) exists = 1;
 	}
 
 	if (exists)
 	{
- 		sleep(time);
-		rotation(directory, log, program, number, 0);
-	} else fprintf(stderr, "Error: el archivo %s no existe\n", log);
-	
+ 		sleep(t);
+		rotation(directory, l, program, number, 0);
+	} else fprintf(stderr, "Error: el archivo %s no existe\n", l);
+
 	closedir(dir);
 }
 
 int main(int argc, char const *argv[]) {
-	int number, time;
+	int number, t;
 	if (argc != 5)
 	{
 		fprintf(stderr, "Forma de uso: %s directorio_log archivo_log numero tiempo\n", argv[0]);
@@ -85,14 +85,14 @@ int main(int argc, char const *argv[]) {
 	}
 
 	number = atoi(argv[3]);
-	time = atoi(argv[4]);
-	if (number <= 0 || time <= 0)
+	t = atoi(argv[4]);
+	if (number <= 0 || t <= 0)
 	{
 		fprintf(stderr, "Error: el programa recibe un numero entero positivo mayor a 0\n");
 		return -1;
 	}
 
-	open_directory(argv[1], argv[0], argv[2], time, number);
+	open_directory(argv[1], argv[0], argv[2], t, number);
 
 	return 0;
 }
